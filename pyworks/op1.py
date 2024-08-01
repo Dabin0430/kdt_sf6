e50694dbde0ad43cd56881a10fb1163f406b0092
Dabin0430 <ydb0430@gmail.com>
2024-08-01 17:21:16 +0900

    파이썬 소스 프로그램

---
+++ pyworks/op1.py
@@ -0,0 +1,25 @@
+# 연산자(operater)
+# 산술연산 - +, -, *, /, //(몫), %(나머지)
+# 입력
+#n1 = 10
+#n2 = 4
+
+# 산술 연산
+
+# 출력
+#print(n1, n2)
+#print(n1 + n2) 
+#print(n1 - n2)
+#print(n1 * n2)
+#print(n1 / n2)
+#print(n1 // n2)
+#print(n1 % n2)
+
+bread = 30
+people = 4
+
+몫 = bread // people
+나머지 = bread % people
+
+print("빵의 개수 :", 몫)
+print("남은 빵의 개수: ", 나머지)
